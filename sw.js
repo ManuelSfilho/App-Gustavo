db5875b14c4957477e424b8af2f52c8d7f4117e3
ManuelSfilho <52848138+ManuelSfilho@users.noreply.github.com>
2026-07-25 18:13:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "filmes-app-v1";
+const CACHE = "filmes-app-v2";
 const SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
